7a674347c881019a034d7d5d3d472c77d1a6c29e
arinaleech <ndjdn6062@gmail.com>
2025-01-03 13:22:33 +0530

    Update lazy_ffmpeg.py

--- database/lazy_ffmpeg.py
+++ database/lazy_ffmpeg.py
@@ -1,6 +1,3 @@
-# with Love @LazyDeveloperr 💘
-# Subscribe YT @LazyDeveloperr - to learn more about this for free...
-
 import time
 import os
 import asyncio
@@ -8,154 +5,96 @@ from PIL import Image
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
+
 async def fix_thumb(c_thumb):
-    width = 0
-    height = 0
+    width, height = 0, 0
     try:
-        if c_thumb != None:
+        if c_thumb is not None:
             metadata = extractMetadata(createParser(c_thumb))
-            if metadata.has("width"):
+            if metadata and metadata.has("width"):
                 width = metadata.get("width")
-            if metadata.has("height"):
+            if metadata and metadata.has("height"):
                 height = metadata.get("height")
-                Image.open(c_thumb).convert("RGB").save(c_thumb)
-                img = Image.open(c_thumb)
-                img.resize((320, height))
-                img.save(c_thumb, "JPEG")
+            img = Image.open(c_thumb).convert("RGB")
+            img = img.resize((320, height))
+            img.save(c_thumb, "JPEG")  # Saving the resized image
     except Exception as e:
-        print(e)
-        c_thumb = None 
+        print(f"Error in fix_thumb: {e}")
+        c_thumb = None
        
     return width, height, c_thumb
-    
+
+
 async def take_screen_shot(video_file, output_directory, ttl):
-     out_put_file_name = output_directory + "/" + str(time.time()) + ".jpg"
+    out_put_file_name = os.path.join(output_directory, f"{str(time.time())}.jpg")
     file_genertor_command = [
-        "ffmpeg",
-        "-ss",
-        str(ttl),
-        "-i",
-        video_file,
-        "-vframes",
-        "1",
-        out_put_file_name
+        "ffmpeg", "-ss", str(ttl), "-i", video_file, "-vframes", "1", out_put_file_name
     ]
-    process = await asyncio.create_subprocess_exec(
-        *file_genertor_command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    if os.path.lexists(out_put_file_name):
-        return out_put_file_name
+    try:
+        process = await asyncio.create_subprocess_exec(
+            *file_genertor_command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await process.communicate()
+        if os.path.lexists(out_put_file_name):
+            return out_put_file_name
+    except Exception as e:
+        print(f"Error in take_screen_shot: {e}")
     return None
+
 
 async def place_water_mark(input_file, output_file, water_mark_file):
     watermarked_file = output_file + ".watermark.png"
-    metadata = extractMetadata(createParser(input_file))
-    width = metadata.get("width")
-    shrink_watermark_file_genertor_command = [
-        "ffmpeg",
-        "-i", water_mark_file,
-        "-y -v quiet",
-        "-vf",
-        "scale={}*0.5:-1".format(width),
-        watermarked_file
-    ]
-    # print(shrink_watermark_file_genertor_command)
-    process = await asyncio.create_subprocess_exec(
-        *shrink_watermark_file_genertor_command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    commands_to_execute = [
-        "ffmpeg",
-        "-i", input_file,
-        "-i", watermarked_file,
-        "-filter_complex",
-        output_file
-    ]
-    # print(commands_to_execute)
-    process = await asyncio.create_subprocess_exec(
-        *commands_to_execute,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    return output_file
+    try:
+        metadata = extractMetadata(createParser(input_file))
+        width = metadata.get("width", 0)
+        shrink_watermark_file_genertor_command = [
+            "ffmpeg", "-i", water_mark_file, "-y", "-v", "quiet",
+            "-vf", f"scale={width}*0.5:-1", watermarked_file
+        ]
+        process = await asyncio.create_subprocess_exec(
+            *shrink_watermark_file_genertor_command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        await process.communicate()
 
-# async def take_screen_shot(video_file, output_directory, ttl):
-# https://stackoverflow.com/a/13891070/4723940
- #   out_put_file_name = output_directory + \
-#         "/" + str(time.time()) + ".jpg"
- #    file_genertor_command = [
- #        "ffmpeg",
-   #      "-ss",
-   #      str(ttl),
-#         "-i",
- #        video_file,
-  #       "-vframes",
-   #      "1",
-    #     out_put_file_name
-#     ]
- #     width = "90"
-  #   process = await asyncio.create_subprocess_exec(
-   #    *file_genertor_command,
-        # stdout must a pipe to be accessible as process.stdout
-#         stdout=asyncio.subprocess.PIPE,
- #        stderr=asyncio.subprocess.PIPE,
-  #   )
-   #  # Wait for the subprocess to finish
-#     stdout, stderr = await process.communicate()
- #    e_response = stderr.decode().strip()
-  #   t_response = stdout.decode().strip()
-   #  if os.path.lexists(out_put_file_name):
-    #     return out_put_file_name
-     #else:
-      #   return None
+        commands_to_execute = [
+            "ffmpeg", "-i", input_file, "-i", watermarked_file,
+            "-filter_complex", "[0][1]overlay=10:10", output_file
+        ]
+        process = await asyncio.create_subprocess_exec(
+            *commands_to_execute,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        await process.communicate()
+        return output_file
+    except Exception as e:
+        print(f"Error in place_water_mark: {e}")
+    return None
+
 
 async def cult_small_video(video_file, output_directory, start_time, end_time):
-    # https://stackoverflow.com/a/13891070/4723940
-    out_put_file_name = output_directory + \
-        "/" + str(round(time.time())) + ".mp4"
+    out_put_file_name = os.path.join(output_directory, f"{round(time.time())}.mp4")
     file_genertor_command = [
-        "ffmpeg",
-        "-i",
-        video_file,
-        "-ss",
-        start_time,
-        "-to",
-        end_time,
-        "-async",
-        "1",
-        "-strict",
-        "-2",
-        out_put_file_name
+        "ffmpeg", "-i", video_file, "-ss", start_time, "-to", end_time,
+        "-async", "1", "-strict", "-2", out_put_file_name
     ]
-    process = await asyncio.create_subprocess_exec(
-        *file_genertor_command,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    if os.path.lexists(out_put_file_name):
-        return out_put_file_name
-    else:
-        return None
+    try:
+        process = await asyncio.create_subprocess_exec(
+            *file_genertor_command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        await process.communicate()
+        if os.path.lexists(out_put_file_name):
+            return out_put_file_name
+    except Exception as e:
+        print(f"Error in cult_small_video: {e}")
+    return None
+
 
 async def generate_screen_shots(
     video_file,
@@ -167,20 +106,19 @@ async def generate_screen_shots(
 ):
     metadata = extractMetadata(createParser(video_file))
     duration = 0
-    if metadata is not None:
-        if metadata.has("duration"):
-            duration = metadata.get('duration').seconds
+    if metadata and metadata.has("duration"):
+        duration = metadata.get('duration').seconds
     if duration > min_duration:
         images = []
         ttl_step = duration // no_of_photos
         current_ttl = ttl_step
-        for looper in range(0, no_of_photos):
+        for _ in range(no_of_photos):
             ss_img = await take_screen_shot(video_file, output_directory, current_ttl)
-            current_ttl = current_ttl + ttl_step
-            if is_watermarkable:
-                ss_img = await place_water_mark(ss_img, output_directory + "/" + str(time.time()) + ".jpg", wf)
+            if ss_img and is_watermarkable:
+                ss_img = await place_water_mark(ss_img, os.path.join(output_directory, f"{str(time.time())}.jpg"), wf)
             images.append(ss_img)
+            current_ttl += ttl_step
         return images
     else:
+        print("Video duration is too short.")
         return None
-
